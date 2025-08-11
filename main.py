921cba56d2d44bbd490c9839ab3591dc7ab7bc99
skuaej <sumankumar821310@gmail.com>
2025-08-11 14:17:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 bot = Client(
     "bot",
-    api_id=27479878,
-    api_hash=da78963da6eaaf521133e00628434271,
-    bot_token=8326037892:AAH52o13ZCPMMMeSvYehHm_o6M2w3JWtl30)
+    api_id="27479878",
+    api_hash="da78963da6eaaf521133e00628434271",
+    bot_token="8326037892:AAH52o13ZCPMMMeSvYehHm_o6M2w3JWtl30")
 
 
 @bot.on_message(filters.command(["start"]))
