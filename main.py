7070b9ae028f51ca4da150edde423e10fbb2da21
skuaej <sumankumar821310@gmail.com>
2025-08-16 18:41:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ bot = Client(
     "bot",
     api_id=24411134,
     api_hash=da78963da6eaaf521133e00628434271,
-    bot_token=8326037892:AAH52o13ZCPMMMeSvYehHm_o6M2w3JWtl30)
+    bot_token=8358162910:AAEUX_QLKp0BYlQdDrPUMzvia4hpuxb3E_M)
 
 
 @bot.on_message(filters.command(["start"]))
